0e37140111bab35f6d48ee6b5ec07b18a421c7d3
MerinPrime <96889060+MerinPrime@users.noreply.github.com>
2026-04-15 15:21:55 +0300

    Add plugins neovim

---
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,43 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add          = { text = "▎" },
+      change       = { text = "▎" },
+      delete       = { text = "" },
+      topdelete    = { text = "" },
+      changedelete = { text = "▎" },
+      untracked    = { text = "▎" },
+    },
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 500,
+      virt_text_pos = "eol",
+    },
+
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      map("n", "]h", function()
+        if vim.wo.diff then return "]h" end
+        vim.schedule(function() gs.next_hunk() end)
+        return "<Ignore>"
+      end, "Next Hunk")
+
+      map("n", "[h", function()
+        if vim.wo.diff then return "[h" end
+        vim.schedule(function() gs.prev_hunk() end)
+        return "<Ignore>"
+      end, "Prev Hunk")
+
+      map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>ghd", gs.diffthis, "Diff This")
+    end,
+  },
+}
