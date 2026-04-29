a53dff4fbe7eceb6cbefd0c7e299b0bb15724856
MerinPrime <96889060+MerinPrime@users.noreply.github.com>
2026-04-29 14:54:29 +0300

    Add configuration to dap ( neovim )

--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -1,3 +1,31 @@
 return {
     "mfussenegger/nvim-dap",
+    config = function()
+        local dap = require("dap")
+
+        dap.adapters.coreclr = {
+            type = "executable",
+            command = "netcoredbg",
+            args = { "--interpreter=vscode" },
+        }
+
+        dap.configurations.cs = {
+            {
+                type = "coreclr",
+                name = "Attach to process",
+                request = "attach",
+                processId = function()
+                    return require("dap.utils").pick_process()
+                end,
+            },
+            {
+                type = "coreclr",
+                name = "Launch file",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+                end,
+            },
+        }
+    end
 }
