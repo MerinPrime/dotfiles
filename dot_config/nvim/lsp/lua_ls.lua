8ceab820a0eedf787b6975d24e21d59915b35be5
MerinPrime <96889060+MerinPrime@users.noreply.github.com>
2026-05-10 22:49:07 +0300

    Test add .luarc.json support in neovim

--- dot_config/nvim/lsp/lua_ls.lua
+++ dot_config/nvim/lsp/lua_ls.lua
@@ -1,15 +1,52 @@
 return {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
+    settings = { Lua = {} },
+    on_init = function(client)
+        local join = vim.fs.joinpath
+        local path = client.workspace_folders[1].name
+        local luarc_path = join(path, '.luarc.json')
+
+        local fd = io.open(luarc_path, "r")
+        if fd then
+            local content = fd:read("*a")
+            fd:close()
+
+            local ok, parsed_json = pcall(vim.json.decode, content)
+
+            if ok and type(parsed_json) == "table" then
+                client.config.settings.Lua = vim.tbl_deep_extend(
+                    'force',
+                    client.config.settings.Lua or {},
+                    parsed_json
+                )
+
+                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                return true
+            else
+                vim.notify("lua_ls: Syntax error in .luarc.json", vim.log.levels.WARN)
+            end
+        end
+
+        local nvim_settings = {
             telemetry = { enable = false },
-        },
-    },
+            runtime = { version = 'LuaJIT' },
+            diagnostics = { globals = { 'vim' } },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    vim.fn.stdpath('config'),
+                },
+            },
+        }
+
+        client.config.settings.Lua = vim.tbl_deep_extend(
+            'force',
+            client.config.settings.Lua or {},
+            nvim_settings
+        )
+
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        return true
+    end,
     filetypes = { "lua" },
 }
